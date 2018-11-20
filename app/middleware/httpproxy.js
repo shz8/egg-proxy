9a5134c4fb3b1a05f59c9c38a5e0658563142aab
张红涛 <zhang.hongtao@zs-hospital.sh.cn>
2018-11-20 14:34:16 +0800

    1

--- app/middleware/httpproxy.js
+++ app/middleware/httpproxy.js
@@ -2,39 +2,40 @@ const url = require('url')
 const HttpProxy = require('http-proxy')
 const proxy = HttpProxy.createProxyServer()
 const util = require('../../lib/utils');
-module.exports = options => {
-  return async function httpproxy(ctx, next) {
-    let proxyTable = (ctx.app.config.httpproxy&&ctx.app.config.httpproxy.proxyTable)||[]
-    let pxy,pathlen=0;
+let proxyTable;
+module.exports = options => async function httpproxy(ctx, next) {
+  if (!proxyTable) {
+    proxyTable = (ctx.app.config.httpproxy && ctx.app.config.httpproxy.proxyTable) || [];
     proxyTable = util.proxySort(proxyTable)
-    for(let idx=0;idx<proxyTable.length;idx++){
-      if(ctx.url.toLowerCase().indexOf(proxyTable[idx].path.toLowerCase().trim())==0){
-        pathlen =proxyTable[idx].path.trim().length;
-        pxy = proxyTable[idx];
-        break;
-      }
+  }
+  let pxy,
+    pathlen = 0;
+  for (let idx = 0; idx < proxyTable.length; idx++) {
+    if (ctx.url.toLowerCase().indexOf(proxyTable[idx].path.toLowerCase().trim()) == 0) {
+      pathlen = proxyTable[idx].path.trim().length;
+      pxy = proxyTable[idx];
+      break;
     }
-    
-    if(pxy)
-    {
-        return new Promise((resolve, reject) => {
-            ctx.req.oldPath = ctx.req.url
-            let u = ctx.req.url.substr(pathlen);
-            if(u) u = u.trim()
-            if(!u)u = '/';
-            ctx.logger.info(u) ;
-            ctx.req.url = u;
-            proxy.web(ctx.req, ctx.res, pxy.proxy, e => {
-                const status = {
-                  ECONNREFUSED: 503,
-                  ETIMEOUT: 504
-                }[e.code]
-                if (status) ctx.status = status
-                resolve()
-            })
-        })
-    }
-    await next();
-    console.log(ctx.url)
-  };
+  }
+
+  if (pxy) {
+    return new Promise((resolve, reject) => {
+      ctx.req.oldPath = ctx.req.url
+      let u = ctx.req.url.substr(pathlen);
+      if (u) u = u.trim()
+      if (!u)u = '/';
+      ctx.logger.info(u);
+      ctx.req.url = u;
+      proxy.web(ctx.req, ctx.res, pxy.proxy, (e) => {
+        const status = {
+          ECONNREFUSED: 503,
+          ETIMEOUT: 504,
+        }[e.code]
+        if (status) ctx.status = status
+        resolve()
+      })
+    })
+  }
+  await next();
+  console.log(ctx.url)
 };
